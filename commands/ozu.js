850408ca3218e8f1b385d610dbeed1bacbe5a669
Chance Lytle <chance_lytle@yahoo.com>
2020-12-14 21:21:21 -0500

    Fixed ozu link

--- commands/ozu.js
+++ commands/ozu.js
@@ -4,6 +4,6 @@ module.exports = {
 	description: 'Request our ozu guide.',
 	args: false,
 	execute(message, args) {
-		message.channel.send(`https://media.discordapp.net/attachments/702861139683311622/785620468420968518/ozuflowcharttm.png?width=313&height=584`);
+		message.channel.send(`https://cdn.discordapp.com/attachments/702861139683311622/785620468420968518/ozuflowcharttm.png`);
 	},
 };
